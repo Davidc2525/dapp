231a10abce98954cd6794a7a0c7e8ef8757473d5
Davidc2525 <david25pcxtreme@gmail.com>
2022-07-27 12:58:23 -0400

    .

---
+++ server/app/prividers/WithDrawProvider/MovilAsyncWithDrawProvider.js
@@ -0,0 +1,56 @@
+import IAsyncWiathDrawProvider from "./IAsyncWiathDrawProvider.js";
+
+
+
+/**
+ * @implements {IAsyncWiathDrawProvider}
+ * 
+ */
+export default class MovilAsyncWithDrawProvider extends IAsyncWiathDrawProvider {
+
+    constructor() {
+        super();
+        console.log("DEBUG MovilAsyncWithDrawProvider");
+    }
+
+
+    /**
+     * 
+     * @param {User} user
+     * @param {Inovice} inovice 
+     * @returns {Inovice}
+     */
+    withdraw(user, inovice) {  }
+
+    /**
+     * Es invocado luego de que el retiro fue
+     * procesado por el servicio correspondiente
+     *
+     * se encarga de actializar los balances del usuario
+     * luego de su procesado (de haber enviado los fondos)
+     *
+     * el servicio solo se encarga de enviar los fondos al usuario
+     * luego este metodo se encarga de actualizar los balances
+     *
+     * @param {string} inoviceid
+     */
+    withdrawalProcessed(inoviceid) { }
+
+
+    /**
+     * se llama cuando el servicio rechaza la retirada de credito
+     * @param {string} inoviceid
+     * @param {string} reason razon por la cual ser rechazo el retiro
+     */
+    withdrawalDeclined(inoviceid, reason) { }
+
+    /**
+     * se llama cuando en el servicio de retiro se produce un error
+     * al momento de enviar los fondos
+     * @param {string} inoviceid
+     * @param {string} err nombre del error
+     * @param {string} msg descripcion del error
+     */
+    withdrawalError(inoviceid, err, msg) { }
+
+}
